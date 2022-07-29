9aea3fe325f7da87e98cae7aa747e4380dbe1611
Clement Bowe <cbowe023@fiu.edu>
2022-07-29 08:21:31 -0400

    implemented second chance algorithm

--- assignment3/second.c
+++ assignment3/second.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include "pa3.h"
+
 #define FRAME_NO 10
 #define lrand() rand()*RAND_MAX + rand()
 int locate(int* frames, int size, int page_no){
@@ -10,12 +12,47 @@ int locate(int* frames, int size, int page_no){
       return i;
   return -1;
 }
-int optimal(int* ref_str, int size, int limit){
+
+
+struct node* front = NULL;
+struct node* rear = NULL;
+
+void enqueue2(int val)
+{
+  struct node* ptr;
+  ptr = (struct node*)malloc(sizeof (struct node));
+  ptr -> value = val;
+  ptr -> next = NULL;
+  
+  if ((front == NULL) && (rear == NULL)) {
+    front = rear = ptr;
+  } else {
+    rear -> next = ptr;
+    rear = ptr;
+  }
+}
+
+int dequeue2() {
+  if(front == NULL) {
+    printf("UNDER!!");
+    return -1;
+  } else {
+    struct node* temp = front;
+    int temp_val = temp->value;
+    front = front->next;
+    free(temp);
+    return temp_val;
+  }
+}
+
+int second(int* ref_str, int size, int limit){
   //size is the # of frames allocated for the process
   //limit is the max # of cells that we look ahead in the RS to implement the optimal algorithm
   //initialize
   int page_faults = size;
-  int frames[size], i, cur, page_no, frame_no;
+  int frames[FRAME_NO], i, cur, page_no, frame_no;
+  int *mod = (int*)malloc(size*sizeof(int));
+
   for(i = 0; i < size;i++)
     frames[i] = -1;//empty
   for(i = 0, cur = 0; i < size;i++,cur++){
@@ -26,6 +63,8 @@ int optimal(int* ref_str, int size, int limit){
       i--;
     else
       frames[i] = page_no;
+      mod[i] = 1;
+      enqueue2(i);
   }
   //main loop
   for(;cur < 1000000;cur++){
@@ -33,29 +72,24 @@ int optimal(int* ref_str, int size, int limit){
     frame_no = locate(frames, size, page_no);
     if(frame_no != -1)//already exists
       continue;
-    //look at ref_str[cur+1:cur+limit] to see which one has not been referred to the longest in the future
-    page_faults++;
-    //This is the very first line that is differnet in other algorithms
-    unsigned unused = (1<<size) - 1;
-    int victim;
-    for(int k = 1; k <= limit && unused && cur + k < 1000000;k++){
-      victim = locate(frames, size, ref_str[cur + k]);
-      if(victim == -1)
-        continue;
-      unused &= ~(1<<victim);
+    
+    int index = dequeue2();
+    while(mod[index] == 1){
+      printf("getting second chance");
+      mod[index]--;
+      enqueue2(index);
+      index = dequeue2();
     }
-    if(!unused)
-      frames[victim] = page_no;
-    else{
-      victim = 0;
-      while(unused % 2 == 0)
-        unused = unused >> 1, victim++;
-      frames[victim] = page_no;
-    }    
+
+    page_faults++;
+    frames[index] = page_no;
+    mod[index] = 1;
+    enqueue2(index);
   } 
-  
   return page_faults;
 }
+
+
 int main(int argc, char** argv) {
   int e, m, P;
   double t;
@@ -99,8 +133,8 @@ int main(int argc, char** argv) {
         locus_position = (locus_position + 1)%(P-e+1);
     }
   }
-  int optimal_page_fault = optimal(ref_str, FRAME_NO, e * m);
-  printf("Optimal page replacement causes %d page faults\n", optimal_page_fault);
+  int second_page_fault = second(ref_str, FRAME_NO, e * m);
+  printf("Second page replacement causes %d page faults\n", second_page_fault);
   return 0;
 }
 
